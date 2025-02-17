c916c340a5d41b1dc8fee37a7180ddc83e38f6d7
Vince Rose <vrose04@gmail.com>
2025-02-17 09:23:06 -0700

    don't print the requests.get

--- ruff-graph/main.py
+++ ruff-graph/main.py
@@ -3,7 +3,7 @@ import requests
 
 lib_c.foo()
 
-print(requests.get('https://www.google.com').text)
+requests.get('https://www.google.com').text
 
 if __name__ == "__main__":
     print("Hello from ruff-graph!")
